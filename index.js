87cb4ea94e7945b0b4e88eafb5a9448b3d7de014
Brian <Bhensley811@gmail.com>
2022-09-19 12:41:47 -0400

    commit tree 9180fb6feaa21252ee13b884769c2d88edb78de6

--- index.js
+++ index.js
@@ -12,5 +12,28 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((string) => {
+
+  let splitArray = string.split(' ')
+  const capitalizedArray = splitArray.map((word) => {
+    return (word.charAt(0).toUpperCase() + word.slice(1))
+  })
+  return capitalizedArray.join(' ')
+})
 }
+
+// console.log(titleCased(tutorials))
+// function splitTutorials(array){
+//   for(const element of array)
+//   return element.split(' ')
+// }
+// function titleCased() {
+//   for(const element of array)
+//   return element.split(' ')
+//   return tutorials.map(element => {
+//     return (element.charAt(0).toUpperCase() + element.slice(1).toLowerCase());
+//   })
+  
+// }
+
+ 
